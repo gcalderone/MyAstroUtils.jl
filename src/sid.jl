7ad1636776ba6d9e960e4faeade79fabe040a772
Giorgio Calderone <giorgio.calderone@inaf.it>
2026-02-13 23:30:43 +0100

    Bugfix

--- src/sid.jl
+++ src/sid.jl
@@ -6,7 +6,7 @@ function SID_conesearch(table::String, id::Vector{Int}, RAd::Vector{T}, DECd::Ve
     @assert length(id) == length(RAd) == length(DECd) == length(radius_arcsec)
     DB("CALL SID.InitSearch('$(table)')")
     for i in 1:length(RAd)
-        DB("CALL SID.AddCone($(id[i]), $(RAd[i]), $(DECd[i]), $(radius_arcsec[i]) / 3600.)")
+        DB("CALL SID.AddCone($(id[i]), $(RAd[i]), $(DECd[i]), $(radius_arcsec[i]) / 60.)")
     end
     sql = DB("SELECT SID.get_query()")
     DB(sql)
